c9795cc3917ad264253b605bfdc6a358062ac83e
Guilherme <guilhermeclarodemorais@gmail.com>
2023-07-28 18:59:06 -0300

    28/07/23 release: package installation/Controller addition

---
+++ jstack/mycontacts/src/app/controllers/ContactController.js
@@ -0,0 +1,23 @@
+class ContactController {
+  index(request, response) {
+    response.send('Hello World');
+  }
+
+  show() {
+
+  }
+
+  store() {
+
+  }
+
+  upgrade() {
+
+  }
+
+  delete() {
+
+  }
+}
+
+module.exports = new ContactController();

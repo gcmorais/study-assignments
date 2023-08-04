f70c5d956788e949314447a86ef555d8acee22b3
Guilherme <guilhermeclarodemorais@gmail.com>
2023-08-04 16:14:03 -0300

    estilizando

--- jstack/react-fundamentos/src/Header.js
+++ jstack/react-fundamentos/src/Header.js
@@ -1,12 +1,13 @@
 import React from "react";
 import PropTypes from 'prop-types';
+import styles from './Header.css';
 
 // Forma desestruturada das props abaixo 
 export function Header({title, subtitle, children}){
     return (
         <>
             <div>
-                <h1>{title}</h1>
+                <h1 className={styles.title}>{title}</h1>
                 <h2>{subtitle}</h2>
                 {children}
             </div>

b0d019146461e4786ef4d53c477de7f2719caf3d
Guilherme Morais <guilhermeclarodemorais@gmail.com>
2023-10-03 20:44:37 -0300

    List contacts build

--- JSTACK/mycontacts/frontend/src/pages/Home/index.js
+++ JSTACK/mycontacts/frontend/src/pages/Home/index.js
@@ -1,50 +1,83 @@
-import React from 'react'
-import { Container, Header, ListContainer, Card,InputSearchContainer } from './styles';
-import arrow from '../../assets/images/icons/arrow.svg';
-import edit from '../../assets/images/icons/edit.svg';
-import trash from '../../assets/images/icons/trash.svg';
-import { Link } from 'react-router-dom/cjs/react-router-dom.min';
-import Modal from '../../components/Modal'
-import Loader from '../../components/Loader';
-
-
+import React, { useEffect, useState } from "react";
+import {
+  Container,
+  Header,
+  ListHeader,
+  Card,
+  InputSearchContainer,
+} from "./styles";
+import arrow from "../../assets/images/icons/arrow.svg";
+import edit from "../../assets/images/icons/edit.svg";
+import trash from "../../assets/images/icons/trash.svg";
+import { Link } from "react-router-dom/cjs/react-router-dom.min";
+import Modal from "../../components/Modal";
+import Loader from "../../components/Loader";
 
 function Home() {
+  const [contacts, setContacts] = useState([]);
+  const [orderBy, setOrderBy] = useState('asc');
+
+  useEffect(() => {
+    fetch(`http://localhost:3001/contacts?orderBy=${orderBy}`)
+      .then(async (response) => {
+        const json = await response.json();
+        setContacts(json);
+      })
+      .catch((error) => {
+        console.log("erro", error);
+      });
+
+    console.log(contacts);
+  }, [orderBy]);
+
+  function handleToggleOrderBy(){
+    setOrderBy((prevState) => prevState === 'asc' ? 'desc' : 'asc')
+  }
+
   return (
     <Container>
-
       <InputSearchContainer>
-        <input type="text" placeholder="Pesquise pelo nome...."/>
+        <input type="text" placeholder="Pesquise pelo nome...." />
       </InputSearchContainer>
       <Header>
-        <h1>3 contatos</h1>
+        <h1>
+          {contacts.length}
+          {contacts.length === 1 ? " contato" : " contatos"}
+        </h1>
         <Link to="/new">Novo contato</Link>
       </Header>
-      <ListContainer>
-        <header>
-          <button type="button">
-            <span>Nome</span>
-            <img src={arrow} alt='arrow' />
-          </button>
-        </header>
-        <Card>
-          <div className='info'>
-            <div className='title'>
-              <strong>Mateus Silva</strong>
-              <small>Instagram</small>
-            </div>
-              <span>mateus@devacademy.com.br</span>
-              <span>(41) 4002-8922</span>
-          </div>
-          <div className='actions'>
-              <Link to="/edit/2"><img src={edit} alt='edit' /></Link>
-              <button type='button'><img src={trash} alt='trash' /></button>
-          </div>
-        </Card>
 
-      </ListContainer>
+      <ListHeader orderBy={orderBy}>
+          <button type="button" onClick={handleToggleOrderBy}>
+            <span>Nome</span>
+            <img src={arrow} alt="arrow" />
+          </button>
+      </ListHeader>
+
+      {contacts.map((item) => (
+          <Card key={item.id}>
+            <div className="info">
+              <div className="title">
+                <strong>{item.name}</strong>
+                {item.category_name && (
+                  <small>{item.category_name}</small>
+                )}
+              </div>
+              <span>{item.email}</span>
+              <span>{item.phone}</span>
+            </div>
+            <div className="actions">
+              <Link to={`/edit/${item.id}`}>
+                <img src={edit} alt="edit" />
+              </Link>
+              <button type="button">
+                <img src={trash} alt="trash" />
+              </button>
+            </div>
+          </Card>
+        ))}
     </Container>
-  )
+  );
 }
 
-export default Home
+export default Home;

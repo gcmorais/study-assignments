9445009f9b6792b20c64e4818024bb30b51861ec
Guilherme Morais <guilhermeclarodemorais@gmail.com>
2023-10-05 17:55:26 -0300

    HttpClient build | Errors handling

--- JSTACK/mycontacts/frontend/src/services/ContactServices.js
+++ JSTACK/mycontacts/frontend/src/services/ContactServices.js
@@ -1,12 +1,11 @@
-import delay from "../utils/delay";
+import HttpClient from './utils/HttpClient'
 
 class ContactServices{
+  constructor(){
+    this.httpClient = new HttpClient('http://localhost:3001');
+  }
   async listContacts(orderBy = 'asc'){
-    const response = await fetch(`http://localhost:3001/contacts?orderBy=${orderBy}`);
-
-    await delay(500);
-
-    return response.json();
+    return this.httpClient.get(`/contacts?orderBy=${orderBy}`);
   };
 }
 
